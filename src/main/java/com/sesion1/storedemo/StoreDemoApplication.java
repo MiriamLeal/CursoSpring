ad633ca044ba1bf7b51f5fab400d0aa39838b16f
Miriam <miriamlealvl@gmail.com>
2021-06-28 17:09:48 -0500

    Initial commit

---
+++ src/main/java/com/sesion1/storedemo/StoreDemoApplication.java
@@ -0,0 +1,17 @@
+package com.sesion1.storedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDemoApplication.class, args);
+	}
+
+
+
+
+
+}
---
+++ src/test/java/com/sesion1/storedemo/StoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesion1.storedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
